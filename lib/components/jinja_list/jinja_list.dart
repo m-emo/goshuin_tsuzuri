1799fcacc8c89ff24129725e63469edfc20416e7
e-emo <shu234prunus87@gmail.com>
2021-09-24 18:05:27 +0900

    修正

--- lib/components/jinja_list/jinja_list.dart
+++ lib/components/jinja_list/jinja_list.dart
@@ -29,80 +29,177 @@ class JinjaList extends StatelessWidget {
           ),
         ),
       ],
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(
-              color: StylesColor.bordercolor,
-              width: 1.0,
-            ))),
-            child: InkWell(
-              // onTap: () => Navigator.pushNamed(context, '/addJinja'),
-              onTap: () {
-                // 神社・寺院別の御朱印一覧取得
-                List<GoshuinListData> goshuinArray = [];
-                for (var getgoshuin in store.goshuinArray) {
-                  // 神社・寺院別のIDが一致する場合、リストに設定
-                  if(getgoshuin.spotId == (store.spotArray)[index].id){
-                    goshuinArray.add(getgoshuin);
-                  }
-                }
+      body: Stack(
+        children: <Widget>[
+          ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(
+                  color: StylesColor.bordercolor,
+                  width: 1.0,
+                ))),
+                child: InkWell(
+                  // onTap: () => Navigator.pushNamed(context, '/addJinja'),
+                  onTap: () {
+                    // 神社・寺院別の御朱印一覧取得
+                    List<GoshuinListData> goshuinArray = [];
+                    for (var getgoshuin in store.goshuinArray) {
+                      // 神社・寺院別のIDが一致する場合、リストに設定
+                      if (getgoshuin.spotId == (store.spotArray)[index].id) {
+                        goshuinArray.add(getgoshuin);
+                      }
+                    }
 
-                // 画面表示用のデータ設定
-                SpotData spotData = (store.spotArray)[index];
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Jinja(store: store, spotData: spotData, goshuinArray:goshuinArray)),
-                );
-              },
-              child: Container(
-                color: Colors.white,
-                padding:
-                    EdgeInsets.only(top: 10, right: 10, bottom: 10, left: 10),
-                height: 85.0,
-                child: Container(
-                  /*color: Colors.white,
+                    // 画面表示用のデータ設定
+                    SpotData spotData = (store.spotArray)[index];
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Jinja(
+                              store: store,
+                              spotData: spotData,
+                              goshuinArray: goshuinArray)),
+                    );
+                  },
+                  child: Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.only(
+                        top: 10, right: 10, bottom: 10, left: 10),
+                    height: 85.0,
+                    child: Container(
+                      /*color: Colors.white,
               padding: const EdgeInsets.only(
                   top: 0.0, right: 10.0, bottom: 0.0, left: 2.0),*/
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        height: 65.0,
-                        width: 95.0,
-                        color: StylesColor.bgImgcolor,
-                        child: Container(
-                            child: showImg((store.spotArray)[index].img),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(right: 10.0),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Text(
-                            "${(store.spotArray)[index].spotName}", // 神社・寺院名
-                            style: Styles.mainTextStyleBold,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            height: 65.0,
+                            width: 95.0,
+                            color: StylesColor.bgImgcolor,
+                            child: Container(
+                              child: showImg((store.spotArray)[index].img),
+                            ),
                           ),
-                        ),
+                          Container(
+                            padding: const EdgeInsets.only(right: 10.0),
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Text(
+                                "${(store.spotArray)[index].spotName}",
+                                // 神社・寺院名
+                                style: Styles.mainTextStyleBold,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            // width: 60,
+                            child: Text(
+                                "[ " +
+                                    "${(store.spotArray)[index].prefectures}" +
+                                    " ]  ",
+                                style: Styles.mainTextStyleSmall),
+                          ),
+                        ],
                       ),
-                      Container(
-                        // width: 60,
-                        child: Text("[ " + "${(store.spotArray)[index].prefectures}" + " ]  ",
-                            style: Styles.mainTextStyleSmall),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+              );
+            },
+            itemCount: (store.spotArray).length,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+                      color: StylesColor.bordercolor,
+                      width: 1.0,
+                    )),
             ),
-          );
-        },
-        itemCount: (store.spotArray).length,
+            height: 1.0,
+          ),
+
+        ],
       ),
+      // body: ListView.builder(
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return Container(
+      //       decoration: BoxDecoration(
+      //           border: Border(
+      //               bottom: BorderSide(
+      //         color: StylesColor.bordercolor,
+      //         width: 1.0,
+      //       ))),
+      //       child: InkWell(
+      //         // onTap: () => Navigator.pushNamed(context, '/addJinja'),
+      //         onTap: () {
+      //           // 神社・寺院別の御朱印一覧取得
+      //           List<GoshuinListData> goshuinArray = [];
+      //           for (var getgoshuin in store.goshuinArray) {
+      //             // 神社・寺院別のIDが一致する場合、リストに設定
+      //             if(getgoshuin.spotId == (store.spotArray)[index].id){
+      //               goshuinArray.add(getgoshuin);
+      //             }
+      //           }
+      //
+      //           // 画面表示用のデータ設定
+      //           SpotData spotData = (store.spotArray)[index];
+      //           Navigator.push(
+      //             context,
+      //             MaterialPageRoute(builder: (context) => Jinja(store: store, spotData: spotData, goshuinArray:goshuinArray)),
+      //           );
+      //         },
+      //         child: Container(
+      //           color: Colors.white,
+      //           padding:
+      //               EdgeInsets.only(top: 10, right: 10, bottom: 10, left: 10),
+      //           height: 85.0,
+      //           child: Container(
+      //             /*color: Colors.white,
+      //         padding: const EdgeInsets.only(
+      //             top: 0.0, right: 10.0, bottom: 0.0, left: 2.0),*/
+      //             child: Row(
+      //               children: <Widget>[
+      //                 Container(
+      //                   height: 65.0,
+      //                   width: 95.0,
+      //                   color: StylesColor.bgImgcolor,
+      //                   child: Container(
+      //                       child: showImg((store.spotArray)[index].img),
+      //                   ),
+      //                 ),
+      //                 Container(
+      //                   padding: const EdgeInsets.only(right: 10.0),
+      //                 ),
+      //                 Expanded(
+      //                   child: Container(
+      //                     child: Text(
+      //                       "${(store.spotArray)[index].spotName}", // 神社・寺院名
+      //                       style: Styles.mainTextStyleBold,
+      //                       overflow: TextOverflow.ellipsis,
+      //                       maxLines: 2,
+      //                     ),
+      //                   ),
+      //                 ),
+      //                 Container(
+      //                   // width: 60,
+      //                   child: Text("[ " + "${(store.spotArray)[index].prefectures}" + " ]  ",
+      //                       style: Styles.mainTextStyleSmall),
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      //   itemCount: (store.spotArray).length,
+      // ),
     );
   }
 }
